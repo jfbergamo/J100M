3aa3d0ce6d61bdb50fc3ee7fee5f4df4d01bce01
J <jacopobergamasco06@gmail.com>
2024-05-13 09:51:27 +0200

    Implementato programma in forma console.

--- src/proj/Corridore.java
+++ src/proj/Corridore.java
@@ -2,6 +2,9 @@
 
 package proj;
 
+import java.util.Random;
+import java.util.ArrayList;
+
 import jlibs.Semaforo;
 
 public class Corridore implements Runnable {
@@ -9,21 +12,65 @@ public class Corridore implements Runnable {
     // ATTRIBUTI
     
     private static Semaforo s = new Semaforo();
+    private static Random r = new Random(System.currentTimeMillis());
     
+    private float r_max, r_min; // Random Range
+    private float vit; // Distanza per vincere
+    private float dist; // Distanza percorsa
+    boolean arrivato;
+
+    private ArrayList<String> scores;
+
     // COSTRUTTORE
 
-    public Corridore() {
-        
+    public Corridore(float min, float max, float vittoria, ArrayList<String> punteggi) {
+        setRMin(min);
+        setRMax(max);
+        setVittoria(vittoria);
+        dist = 0;
+        arrivato = false;
+        scores = punteggi;
     }
 
     // METODI
 
     @Override
     public void run() {
+        while (!arrivato) {
+            s.P();
 
+            dist += r.nextFloat(r_min, r_max + 1);
+            if (dist >= vit) {
+                arrivato = true;
+                scores.add(Thread.currentThread().getName());
+            }
+            System.out.println(Thread.currentThread().getName() + ": " + dist);
+            s.V();
+        }
     }
 
     // GETTER
+    
+    public float getVittoria() {
+        return vit;
+    }
+
+    public float getDist() {
+        return dist;
+    }
 
     // SETTER
+
+    private void setRMin(float m) {
+        r_min = m;
+    }
+
+    private void setRMax(float M) {
+        r_max = M;
+    }
+
+    public void setVittoria(float vittoria) {
+        vit = vittoria;
+    }
+
 }
--- src/proj/Demo.java
+++ src/proj/Demo.java
@@ -2,10 +2,49 @@
 
 package proj;
 
-import jlibs.Semaforo;
+import java.util.ArrayList;
 
 public class Demo {
+
+    private static ArrayList<String> scores = new ArrayList<String>();
+    private static final String[] nomi = {"Riccardo", "Gianpippo", "Pierpaolo", "Yudri", "Owusu"};
+    
+    private static final int corridori = nomi.length;
+    private static Thread[] ts = new Thread[corridori];
+    private static Corridore[] cs = new Corridore[corridori];
+
+    private static float min = 0.50f;
+    private static float max = 1.30f;
+    private static float vittoria = 100.0f;
+
     public static void main(String[] args) {
-        System.out.println("Ciao, Nunzio!");
+        InitAll();
+        StartAll();
+        WaitAll();
+
+        for (String c : scores) {
+            System.out.println((scores.indexOf(c) + 1) + "] " + c);
+        }
+    }
+
+    private static void InitAll() {
+        for (int i = 0; i < corridori; i++) {
+            cs[i] = new Corridore(min, max, vittoria, scores);
+            ts[i] = new Thread(cs[i], nomi[i]);
+        }
+    }
+
+    private static void StartAll() {
+        for (Thread t : ts) {
+            t.start();
+        }
+    }
+
+    private static void WaitAll() {
+        for (Thread t : ts) {
+            try {
+                t.join();
+            } catch (InterruptedException ex) { System.exit(ex.hashCode()); }
+        }
     }
 }
